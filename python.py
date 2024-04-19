e2b280aabe6c093b7b9d62bc92ea29644301370e
alexis <alexisalazarmanama@hotmail.com>
2024-04-19 11:39:39 -0400

    raiz cuadrada

---
+++ python.py
@@ -0,0 +1,11 @@
+# condicionales
+num = float( input( "Ingrese un número: ") )
+
+# if condición: 
+    #hago algo 
+
+if ( num >= 10) and ( num <= 20 ) :
+    print("entre 10 y 20")
+
+else: 
+    print("fuera de rango")
